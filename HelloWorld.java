9d0c1992e75fdbeac80667a0c790729f1b98a96b
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-09-07 11:34:00 -0700

    Initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("was good dawg") ;
+        int i;
+        double f = 2.5;
+        boolean flag = true; 
     }
 }
-
